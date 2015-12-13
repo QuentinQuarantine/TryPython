79e0669c76faa2485ef5e7fa308104814ff9ed0e
iury <satriani-16@hotmail.com>
2015-12-13 00:28:57 -0200

    primeiro commit

---
+++ app.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+
+import json
+import sys
+import code
+
+from io import BytesIO
+from tornado import ioloop, web, template, gen
+
+@gen.coroutine
+def _eval(request):
+    ns = {}
+    to_eval = json.loads(request.body)['toEval']
+    out, err = sys.stdout, sys.stderr
+    fake_stdout, fake_stderr = BytesIO(), BytesIO()
+    sys.stdout, sys.stderr = fake_stdout, fake_stderr
+    console = code.InteractiveConsole(locals=ns)
+    for statement in to_eval.split("\n"):
+        if statement:
+            console.push(statement)
+        else:
+            console.push('\n')
+    sys.stdout, sys.stderr = out, err
+
+    out_result, err_result = fake_stdout.getvalue(), fake_stderr.getvalue()
+
+    return {"out": out_result,
+           "err": err_result
+           }
+
+
+loader = template.Loader('templates')
+
+class MainHandler(web.RequestHandler):
+    def get(self):
+        self.write(loader.load("main.html").generate())
+
+
+class EvalHandler(web.RequestHandler):
+
+    @gen.coroutine
+    def post(self):
+        response = yield _eval(self.request)
+        self.write(response)
+
+
+def make_app():
+    return web.Application([
+        (r"/", MainHandler),
+        (r"/eval", EvalHandler),
+        (r'/static/(.*)', web.StaticFileHandler, {'path': 'static/'}),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8080)
+    ioloop.IOLoop.current().start()
---
+++ static/python_console.js
@@ -0,0 +1,76 @@
+var multiple_statements = "";
+
+var get_last_statement = function(line){
+    var splited_line = line.split("\n");
+    return splited_line[splited_line.length -1];
+};
+
+$(document).ready(function(){
+         
+var console2 = $('<div class="container">');
+$('body').append(console2);
+var controller2 = console2.console({
+ promptLabel: '>>> ',
+ continuedPromptLabel: '...\t',
+ commandValidate:function(line){
+    return true;
+ },
+ commandHandle:function(line, report){
+    console.log(line);
+    if (controller2.continuedPrompt){
+        var last_statement = get_last_statement(line);
+        if (last_statement.trim()){
+            if (!last_statement.endsWith(":")){
+                multiple_statements += '\n\t' + last_statement;
+            }else if(last_statement.endsWith(":")){
+                multiple_statements += '\n' + last_statement;
+            }
+            return;
+        }
+    }
+    if (line.endsWith(":")){
+      controller2.continuedPrompt = true;
+      multiple_statements += get_last_statement(line);
+      return;
+    }else{
+      controller2.continuedPrompt = false;
+    }
+     if (multiple_statements){
+        line = multiple_statements + '\n';
+        multiple_statements = "";
+     }
+     $.ajax({
+        url: 'http://localhost:8080/eval',
+        dataType: 'json',
+        type: 'POST',
+        data: JSON.stringify({toEval: line}),
+        success: function(result){
+          var msgs = [];
+          var out = result.out;
+          var err = result.err;
+          console.log("out: " + out);
+          console.log("error: " + err);
+          if (err){
+            report([
+                {msg: err,
+                className: "jquery-console-error"
+                }]);
+                return;
+          }
+
+          out = out.split("\n");
+          for (var l in out){
+            line_out = out[l];
+            if (line_out.trim()){  
+              msgs.push({msg:" " + line_out,
+                        className: "jquery-console-message-value"
+                        });
+            }
+          }
+          report(msgs);
+        }
+      });
+ },
+ welcomeMessage:'Welcome to python interactive web console.'
+});
+});
