c026fdff1889ae5498501834aac0b6e44ac05a0f
iury <satriani-16@hotmail.com>
2015-12-17 01:18:15 -0200

    js tests

--- TryPython/try_python_tutorial/static/js/python_console.js
+++ TryPython/try_python_tutorial/static/js/python_console.js
@@ -1,49 +1,50 @@
-var multiple_statements = "";
+var py_console = (function(){
 
-ajax.initAjax($);
+  // private
+  var _multiple_statements = "";
 
-var get_last_statement = function(line){
-    var splited_line = line.split("\n");
-    return splited_line[splited_line.length -1];  
-};
+  var _get_last_statement = function(line){
+      var splited_line = line.split("\n");
+      return splited_line[splited_line.length -1];  
+  };
 
-$(document).ready(function(){
-         
-var console_ = $('<div class="console">');
-$('body').append(console_);
-var controller2 = console_.console({
- promptLabel: '>>> ',
- continuedPromptLabel: '...\t',
- commandValidate:function(line){
+
+  //public
+  var api = {};
+  api.console_options = {};
+
+  api.console_options.commandValidate = function(line){
     return true;
- },
- commandHandle:function(line, report){
-   
-    if (controller2.continuedPrompt){
-        var last_statement = get_last_statement(line);
+  };
+
+  api.console_options.promptLabel = ">>> ";
+  api.console_options.continuedPromptLabel = '...\t';
+  api.console_options.commandHandle = function(line, report){
+    if (api.console.continuedPrompt){
+        var last_statement = _get_last_statement(line);
         if (last_statement.trim()){
             if (!last_statement.endsWith(":")){
-                multiple_statements += '\n\t' + last_statement;
+                _multiple_statements += '\n\t' + last_statement;
             }else if(last_statement.endsWith(":")){
-                multiple_statements += '\n' + last_statement;
+                _multiple_statements += '\n' + last_statement;
             }
             return;
         }
 
     }
     if (line.endsWith(":")){
-      controller2.continuedPrompt = true;
-      multiple_statements += get_last_statement(line);
+      api.console.continuedPrompt = true;
+      _multiple_statements += _get_last_statement(line);
       return;
     }else{
-      controller2.continuedPrompt = false;
+      api.console.continuedPrompt = false;
     }
-     if (multiple_statements){
-        line = multiple_statements + '\n';
-        multiple_statements = "";
+     if (_multiple_statements){
+        line = _multiple_statements + '\n';
+        _multiple_statements = "";
      }
 
-     ajax.ajax($, "/eval", {toEval: line}, function(result){
+     api.try_python_rest_api.eval($, "/eval", {toEval: line}, function(result){
         var msgs = [];
           var out = result.out;
           var err = result.err;
@@ -68,8 +69,23 @@ var controller2 = console_.console({
             return;
           }
      });
- },
- welcomeMessage: 'Welcome to python interactive web console.',
- autofocus: true
-});
-});
+    };
+  api.console_options.welcomeMessage = 'Welcome to python interactive web console.';
+  api.console_options.autofocus = true;
+  
+  api.init = function(jquery, try_python_rest_api){
+    api.jquery = jquery;
+    api.try_python_rest_api = try_python_rest_api;
+    try_python_rest_api.init(jquery);
+
+    api.jquery(document).ready(function(){
+      var console_element = api.jquery('<div class="console">');
+      api.jquery('body').append(console_element);
+
+      api.console = console_element.console(api.console_options);
+    });
+  };
+
+  return api;
+})();
+
