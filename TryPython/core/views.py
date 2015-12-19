1a6259debfd15599bdb06709f8766d690594ccad
iury <satriani-16@hotmail.com>
2015-12-19 18:46:53 -0200

    remove unused imports

--- TryPython/core/views.py
+++ TryPython/core/views.py
@@ -1,9 +1,7 @@
 # coding: utf-8
 
-import json
 from StringIO import StringIO
 from django.core.management import call_command
-from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from django.http import JsonResponse, HttpResponse
 
