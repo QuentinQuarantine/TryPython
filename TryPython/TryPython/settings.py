bf8841cc4c1b84fc5842e115f5b9d9121ec8d858
iury <satriani-16@hotmail.com>
2015-12-19 18:28:58 -0200

    moving templates folder to "core" app folder

--- TryPython/TryPython/settings.py
+++ TryPython/TryPython/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TryPython.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
