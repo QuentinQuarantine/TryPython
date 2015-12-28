328781d805548bb4018bcfa72fe2459737967eca
iury <satriani-16@hotmail.com>
2015-12-27 23:16:28 -0200

    convert tabs to spaces

--- TryPython/core/models.py
+++ TryPython/core/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Step(models.Model):
-	title = models.TextField()
-	content = models.TextField()
+    title = models.TextField()
+    content = models.TextField()
 
-	def to_dict(self):
-		return {
-			'content': self.content,
-			'title': self.title
-        }
+    def to_dict(self):
+        return {
+            'content': self.content,
+            'title': self.title
+}
