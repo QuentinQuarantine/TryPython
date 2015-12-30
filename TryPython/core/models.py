cdc2e32fc062e5562d8d35ea0b183be2e2e63d35
iury <satriani-16@hotmail.com>
2015-12-30 15:15:57 -0200

    pep8

--- TryPython/core/models.py
+++ TryPython/core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Step(models.Model):
     title = models.TextField()
     content = models.TextField()
+
     def to_dict(self):
         return {
             'content': self.content,
