0801408a1691caa9bbfab4ad654f65b232fdb7ea
iury <satriani-16@hotmail.com>
2016-01-16 00:09:41 -0200

    more tests

--- TryPython/core/tests/test_commands.py
+++ TryPython/core/tests/test_commands.py
@@ -1,6 +1,8 @@
+import sys
 import os
 import json
 from StringIO import StringIO
+from subprocess import CalledProcessError
 
 import six
 
@@ -16,23 +18,38 @@ from django.core.management import call_command, CommandError
 
 class CommandsTests(TestCase):
 
-    def test_eval_command_with_only_pypy_interact_option(self):
-        namespace = json.dumps({})
-        out = StringIO()
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self.namespace = json.dumps({})
+        self.out = StringIO()
+        self.fake_stderr = StringIO()
+        self.original_stderr = sys.stderr
+        sys.stderr = self.fake_stderr
 
+    def tearDown(self):
+        super(self.__class__, self).tearDown()
+        sys.stderr = self.original_stderr
+
+    def test_eval_command_with_only_pypy_interact_option(self):
         with self.assertRaises(CommandError):
-            call_command('eval', '1+1', namespace, pypy_interact="pypy_interact.py", stdout=out)
+            call_command('eval', '1+1', self.namespace,
+                         pypy_interact="pypy_interact.py", stdout=self.out)
+
+    @patch('subprocess.check_output')
+    def test_subprocess_raises_called_error(self, check_output):
+        check_output.side_effect = CalledProcessError(2, "<console>")
+
+        call_command('eval', '1+1', self.namespace)
+        self.assertEqual(self.fake_stderr.getvalue(), "Command '<console>' returned non-zero exit status 2")
 
     @patch('subprocess.check_output')
     def test_eval_command_with_all_options_to_run_in_pypy(self, check_output):
-        namespace = json.dumps({})
-        out = StringIO()
 
-        call_command('eval', '1+1', namespace,
+        call_command('eval', '1+1', self.namespace,
                      pypy_interact="pypy_interact.py",
                      tmp_folder="TryPython/",
                      pypy_c="/usr/bin/pypy-c",
-                     stdout=out)
+                     stdout=self.out)
 
         eval_script_path = os.path.join(settings.BASE_DIR, 'eval.py')
         check_output.assert_called_with(
