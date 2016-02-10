5dd64decc5e91539f6f8cc2f555b27b504936a07
iury <satriani-16@hotmail.com>
2016-02-10 01:26:26 -0200

    fixing tests

--- TryPython/core/tests/test_views.py
+++ TryPython/core/tests/test_views.py
@@ -15,10 +15,11 @@ class EvalViewTestCase(TestCase):
 
     def test_eval_view_simple_expression(self):
         response = self.client.post("/eval", {"toEval": "1+1"})
-        expected = json.dumps({"out": "2\n", "err": ""}).encode("utf-8")
+        expected = {"out": "2\n", "err": ""}
+        decoded_response = json.loads(response.content.decode("utf-8"))
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.content, expected)
+        self.assertEquals(decoded_response, expected)
 
     def test_eval_view_name_binding(self):
         response = self.client.post("/eval", {"toEval": "a = 1"})
@@ -27,36 +28,41 @@ class EvalViewTestCase(TestCase):
 
         response = self.client.post("/eval", {"toEval": "a"})
 
-        expected = json.dumps({u'err': u'', u'out': u'1\n'}).encode("utf-8")
+        expected = {u'err': u'', u'out': u'1\n'}
+        decoded_response = json.loads(response.content.decode('utf-8'))
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.content, expected)
+        self.assertEquals(decoded_response, expected)
 
     def test_eval_view_for_statement(self):
         response = self.client.post(
             "/eval", {"toEval": "for x in (1, 2):\n  print(x)\n"})
-        expected = json.dumps({u'err': u'', u'out': u'1\n2\n'}).encode("utf-8")
-
+        expected = {u'err': u'', u'out': u'1\n2\n'}
+        decoded_response = json.loads(response.content.decode("utf-8"))
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.content, expected)
+        self.assertEquals(decoded_response, expected)
 
     def test_eval_view_while_loop(self):
         response = self.client.post(
             "/eval", {"toEval": "while 1:\n  print('hey')\n  break\n"})
-        expected = json.dumps({"out": "hey\n", "err": ""}).encode("utf-8")
+        expected = {"out": "hey\n", "err": ""}
+        decoded_response = json.loads(response.content.decode("utf-8"))
 
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.content, expected)
+        self.assertEquals(decoded_response, expected)
 
     def test_eval_view_that_will_raise_python_exception(self):
         response = self.client.post("/eval", {'toEval': 'a'})
-        expected = json.dumps(
-            {"out": "",
-             "err": "Traceback (most recent call last):\n  File \"<console>\","
+        expected = {"out": "",
+                    "err": "Traceback (most recent call last):\n"
+                    "  File \"<console>\","
                     " line 1, in <module>\nNameError: name \'a\' is not"
-                    " defined\n"}).encode("utf-8")
+                    " defined\n"
+                    }
+        decoded_response = json.loads(response.content.decode("utf-8"))
+
         self.assertEquals(response.status_code, 200)
-        self.assertEquals(response.content, expected)
+        self.assertEquals(decoded_response, expected)
 
     def test_eval_view_function_statement(self):
         response = self.client.post(
